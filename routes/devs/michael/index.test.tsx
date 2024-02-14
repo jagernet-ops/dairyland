129c25ef2313b770ff3f9bf54ae8e13a71314d47
Michael <mshalewski@outlook.com>
2024-02-14 12:30:23 -0600

    changed about to dev file structure

---
+++ routes/devs/michael/index.test.tsx
@@ -0,0 +1,12 @@
+import { render } from "../../../testing-deps.ts";
+import { describe, it } from "../../../testing-deps.ts";
+import TestRoute from "./index.tsx"
+import { assertSnapshot } from "$fresh/src/server/deps.ts";
+
+describe("about endpoint test", function(){
+  it("test against snapshot", async function(snapshot): Promise<void> {
+    const renderedEndpoint: string = render(<TestRoute />); 
+    await assertSnapshot(snapshot, renderedEndpoint);
+  });
+});
+

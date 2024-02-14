95d68dad18babc3e72979bc64c440a9eba6b21c7
Michael <mshalewski@outlook.com>
2024-02-14 12:35:47 -0600

    updated interests

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,8 +4,8 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $about from "./routes/about.tsx";
 import * as $blog from "./routes/blog.tsx";
+import * as $devs_michael_index from "./routes/devs/michael/index.tsx";
 import * as $index from "./routes/index.tsx";
 import * as $set_title_index from "./islands/set-title/index.tsx";
 import { type Manifest } from "$fresh/server.ts";
@@ -14,8 +14,8 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/about.tsx": $about,
     "./routes/blog.tsx": $blog,
+    "./routes/devs/michael/index.tsx": $devs_michael_index,
     "./routes/index.tsx": $index,
   },
   islands: {
